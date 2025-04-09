9b430a4e5d4a8ed8d01c1e3310e5d43ed5391f4f
elad <elad@cloudents.com>
2025-04-09 20:36:11 +0300

    use jinja to use render_template

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "<h1>Hello world!</h1>"
+	return render_template('index.html', message="Hello world!")
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About', description='This is a flask demo app')
+	
 
 if (__name__ == "__main__"):
 	app.run(debug=True)
